65043a33478ecd72cdb55799d1a59887e9887a10
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-03-12 22:23:19 +0530

    add new files

---
+++ 01_basic/one.py
@@ -0,0 +1,17 @@
+
+print("he hi, i am laxman shinde")
+
+# -------------------- x --------------------
+
+fullName = "laxman shinde"
+
+name = fullName
+fullName = "lakhan patil"
+
+print("the name of fullName :", fullName)
+
+print("the name of name :", name)
+
+# -------------------- x --------------------
+
+
