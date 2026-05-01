1b08ee49c9abeb0ab5e34c9708e584e1955b8ac0
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-05-02 01:03:56 +0530

    chore(strings): add basic string manipulation examples
    - Add examples for capitalize, casefold, count, endswith, find, isupper, and replace methods
    - Include commented sample outputs and explanations
    - Remove obsolete files with simple print statements and variable usage examples

---
+++ 01_basic/string.py
@@ -0,0 +1,54 @@
+# ====================
+#          String
+# ====================
+
+
+firstName = "laxman shinde"
+
+# ----------------------------------------------
+
+# firstName = firstName[0]
+# print(firstName)
+
+# ----------------------------------------------
+
+# firstName = firstName[0]
+# print(firstName)
+
+# ----------------------------------------------
+
+# firstName = firstName.capitalize() # it uppercase the first latter of word
+# print(firstName)
+
+# ----------------------------------------------
+
+# firstName = firstName.casefold()
+# print(firstName)
+
+# ----------------------------------------------
+
+# firstName = firstName.count("n") #specify the character reapets times
+# print(firstName)
+
+# ----------------------------------------------
+
+# firstName = firstName.endswith("j") #return the boolean type but it's denots like what string ends with of character
+# print(firstName)
+
+# ----------------------------------------------
+
+# firstName = firstName.find("s") # return index number of character which are u give
+# print(firstName)
+
+# ----------------------------------------------
+
+# firstName = firstName.isupper() # when full of string are in uppercase ( == like LAXMAN SHINDE all character of string in CAPITAL ) then it will give you return true otherwise false
+# print(firstName)
+
+# ----------------------------------------------
+
+# firstName = firstName.replace("laxman","lakhan") # it will gives us two value first one was old what's you change then second one what you wan't
+
+# ----------------------------------------------
+
+
