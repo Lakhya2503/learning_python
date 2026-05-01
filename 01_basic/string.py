c6de2b35edc0597368cfa55efb75a0ee8b989ea2
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-05-02 02:51:10 +0530

    feat(basic): add list and string examples with explanations
    - Added list operations examples including clear, copy, count, insert, index, extend, pop, remove, reverse, sort, and length
    - Included usage examples for string methods such as capitalize, casefold, count, endswith, find, isupper, replace, slicing, and concatenation
    - Provided comments explaining each list and string method for better understanding
    - Added sample list and string variables to demonstrate Python built-in types and methods
    - Created initial empty tuple file for future tuple examples

--- 01_basic/string.py
+++ 01_basic/string.py
@@ -1,13 +1,16 @@
 # ====================
-#          String
+#          STRING
 # ====================
 
 
 firstName = "laxman shinde"
 
+
 # ----------------------------------------------
 
-# firstName = firstName[0]
+# anotherName = " lakhan shinde"
+
+# firstName = firstName + anotherName
 # print(firstName)
 
 # ----------------------------------------------
@@ -17,7 +20,12 @@ firstName = "laxman shinde"
 
 # ----------------------------------------------
 
-# firstName = firstName.capitalize() # it uppercase the first latter of word
+# firstName = firstName[0]
+# print(firstName)
+
+# ----------------------------------------------
+
+# firstName = firstName.capitalize()  # it uppercase the first latter of word
 # print(firstName)
 
 # ----------------------------------------------
@@ -27,28 +35,47 @@ firstName = "laxman shinde"
 
 # ----------------------------------------------
 
-# firstName = firstName.count("n") #specify the character reapets times
+# firstName = firstName.count("n")  # specify the character reapets times
 # print(firstName)
 
 # ----------------------------------------------
 
-# firstName = firstName.endswith("j") #return the boolean type but it's denots like what string ends with of character
+# firstName = firstName.endswith("j")  # return the boolean type but it's denots like what string ends with of character
 # print(firstName)
 
 # ----------------------------------------------
 
-# firstName = firstName.find("s") # return index number of character which are u give
+# firstName = firstName.find("s")  # return index number of character which are u give
 # print(firstName)
 
 # ----------------------------------------------
 
-# firstName = firstName.isupper() # when full of string are in uppercase ( == like LAXMAN SHINDE all character of string in CAPITAL ) then it will give you return true otherwise false
+# firstName = firstName.isupper()   # when full of string are in uppercase ( == like LAXMAN SHINDE all character of string in CAPITAL ) then it will give you return true otherwise false
 # print(firstName)
 
 # ----------------------------------------------
 
-# firstName = firstName.replace("laxman","lakhan") # it will gives us two value first one was old what's you change then second one what you wan't
+# firstName = firstName.replace("laxman","lakhan")   # it will gives us two value first one was old what's you change then second one what you wan't
 
 # ----------------------------------------------
 
+# firstName = firstName[2:6]   # simply give starting index and last index + 1 he didn't conclude the last number ( ** like firstName = "laxman" but you wan't only "lax" then you give like firstName[0:3] then he will return the lax )
 
+# print(firstName)
+
+# ----------------------------------------------
+
+# firstName = firstName[:]   # when you didn't give a starting index and ending index then he will start will with 0 th index and goas with end
+# print(firstName)
+
+# ----------------------------------------------
+
+# firstName = firstName[2:]   # when you give you starting index but didn't tell the ending index then he will goes on ending index
+# print(firstName)
+
+# ----------------------------------------------
+
+# firstName = firstName[:3]  # when you didn't give the starting index then he will assume the start from the 0 th index
+# print(firstName)
+
+# ----------------------------------------------
